0650f6b6dd38f37da9e2a36475c778998a25d4d6
Artem Bolotov <teeemon@gmail.com>
2023-02-16 09:51:42 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by artembolotov on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
